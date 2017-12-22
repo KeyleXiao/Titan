2f1bfd85c01845cba493fbe369c364b63afa502f
Joey <85789685@qq.com>
2017-12-22 17:17:40 +0800

    init

--- Bin/Manager/CloseManagerServer.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-taskkill /f /im ManagerClient.exe
-taskkill /f /im ManagerServer.exe
-taskkill /f /im MainManagerServer.exe
-taskkill /f /im RootManagerServer.exe
-
-exit
--- Bin/Tools/GenurateMDB/U3D_SceneRender.bat
+++
@@ -1,8 +0,0 @@
-rem 项目的Assets路径
-set ProjectDir="F:\Speed\Bin\Client\Game\Assets"
-rem Unity安装目录
-set UnityDir="E:\Unity5.4.2p4-32bit\Unity\Editor\Data\MonoBleedingEdge"
-
-%UnityDir%\bin\cli.bat %UnityDir%\lib\mono\4.5\pdb2mdb.exe %ProjectDir%\Scripts\U3D_SceneRender.dll
-
-exit
